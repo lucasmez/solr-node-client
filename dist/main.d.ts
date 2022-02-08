4846f2fc8bd05d54753c1b49b7a29dc653ff3b7a
Lucas Mezalira <lucasmezalira@hotmail.com>
2022-02-07 19:27:43 -0500

    Add dist folder so that the package can be installed from github instead of npm

---
+++ dist/main.d.ts
@@ -0,0 +1,5 @@
+export { createClient, Client } from './lib/solr';
+export { Query } from './lib/query';
+export type { QueryOptions } from './lib/query';
+export { Collection } from './lib/collection';
+export type { SolrClientParams, ResourceOptions } from './lib/types';
